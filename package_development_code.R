3d40984e9bfaafe39413a439c527d55e9180ba8e
Leon Steinmeier <Lenostatos@gmx.de>
2020-04-25 15:58:26 +0200

    Add Rcpp to the package.

--- package_development_code.R
+++ package_development_code.R
@@ -1,9 +1,9 @@
-# Set up renv for package management
+# Set up renv for package management.
 install.packages("remotes")
 remotes::install_github("rstudio/renv")
 renv::init()
 
-# Install packages needed for development
+# Install packages needed for development.
 install.packages(c("roxygen2", "testthat", "knitr"))
 remotes::install_github("r-lib/devtools")
 remotes::install_github("r-lib/usethis")
@@ -12,22 +12,20 @@ install.packages("styler")
 # Make usethis functions available on the command line without package prefix.
 # usethis::use_usethis()
 
-# Create bare-bones package structure
+# Create bare-bones package structure.
 usethis::create_package(".")
 
 # Make usethis functions available on the command line without package prefix.
 # usethis::use_usethis()
 
-# Ignore this file
+# Ignore this file.
 usethis::use_build_ignore("package_development_code.R")
 
-# Add a license
+# Add a license.
 usethis::use_gpl3_license(name = "Nikolai Knapp")
 
-# Install packages that this package uses
-usethis::use_package("assertthat")
-
+# Add C++ code.
 usethis::use_rcpp()
-usethis::use_r(name = "crownsegmentr-package.R")
+usethis::use_r(name = "meanshiftr-package.R")
 # The following stackoverflow post provides valuable info on the Makevars file:
 # https://stackoverflow.com/questions/43597632/understanding-the-contents-of-the-makevars-file-in-r-macros-variables-r-ma
