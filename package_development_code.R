3789a2705e69e8ccb4b8d5467af29e5a22eb6aa6
Leon Steinmeier <Lenostatos@gmx.de>
2020-04-25 12:59:05 +0200

    Update license with correct rights holder.

--- package_development_code.R
+++ package_development_code.R
@@ -22,29 +22,12 @@ usethis::create_package(".")
 usethis::use_build_ignore("package_development_code.R")
 
 # Add a license
-usethis::use_gpl3_license(name = "Leon Steinmeier")
+usethis::use_gpl3_license(name = "Nikolai Knapp")
 
 # Install packages that this package uses
 usethis::use_package("assertthat")
-
-install.packages("dbscan")
-usethis::use_package("dbscan")
-
-install.packages("data.table")
-usethis::use_package("data.table")
-
-usethis::use_package("BH", type = "LinkingTo")
 
 usethis::use_rcpp()
 usethis::use_r(name = "crownsegmentr-package.R")
 # The following stackoverflow post provides valuable info on the Makevars file:
 # https://stackoverflow.com/questions/43597632/understanding-the-contents-of-the-makevars-file-in-r-macros-variables-r-ma
-
-devtools::document()
-# Or use keyboard shortcut Ctrl + Shift + D
-
-usethis::use_r(name = "try_to_extract_coordinate_data")
-# Execute usethis::use_test() interactively when the newly created R file is
-# active in order to create an R file with tests.
-
-usethis::use_r(name = "segment_tree_crowns")
